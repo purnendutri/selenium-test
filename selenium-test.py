82a0364dd081e6238d8b2d3d1cfe787191fedbb7
purnendutri <92241821+purnendutri@users.noreply.github.com>
2021-10-10 20:08:03 +0530

    updating xpath

--- selenium-test.py
+++ selenium-test.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time, os
 
 driver = webdriver.Chrome(r"chromedriver.exe")
@@ -11,20 +12,29 @@ driver.get("https://github.com/login")
 my_username = os.getenv('userid',"purnendutri")
 my_password= os.getenv('password',"Jinu9876")
 
-username_input_box = driver.find_element_by_name("login")
-password_input_box = driver.find_element_by_name("password")
-login_button = driver.find_element_by_name("commit")
+#Print Element value
+webElement = driver.find_element_by_link_text("Create an account")
+print(webElement.text)
+print(webElement.location)
 
-#clear the placeholders data
-username_input_box.clear()
-password_input_box.clear()
+try:
+    username_input_box = driver.find_element_by_name("login")
+    password_input_box = driver.find_element_by_name("password")
+    login_button = driver.find_element_by_xpath("(//input[starts-with(@name, 'commit')])")
 
-#fill login credentials
-username_input_box.send_keys(my_username)
-time.sleep(2) 
-password_input_box.send_keys(my_password)
-time.sleep(2)
-login_button.click()
+    #clear the placeholders data
+    username_input_box.clear()
+    password_input_box.clear()
+
+    #fill login credentials
+    username_input_box.send_keys(my_username)
+    time.sleep(2) 
+    password_input_box.send_keys(my_password)
+    time.sleep(2)
+    login_button.click()
+
+except NoSuchElementException as exception:
+    print ("Element not found and test failed")
 
 time.sleep(15)
 driver.close()
